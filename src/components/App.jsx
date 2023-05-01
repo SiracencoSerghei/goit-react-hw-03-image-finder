e02b09e02f52c4c04273f3290cd0627eb01c7de0
Siracenco Serghei <siracencoserghei@gmail.com>
2023-05-01 20:54:34 +0200

    at the request of the mentor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import ImageGallery from './ImageGallery';
 import Button from './Button';
 import Loader from './Loader';
 import Modal from './Modal';
-import fetchImagesWithQuery from '../services/fetchAPI';
+import {fetchImagesWithQuery, param} from '../services/fetchAPI';
 
 class App extends Component {
   state = {
@@ -15,6 +15,7 @@ class App extends Component {
     endOfResults: false,
     isModalOpen: false,
     selectedImage: '',
+    totalPhoto: 0,
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -29,12 +30,15 @@ class App extends Component {
     const { searchQuery, page } = this.state;
     this.setState({ loading: true });
     fetchImagesWithQuery(searchQuery, page)
-      .then((images) =>
+      .then((data) =>{
+        console.log(data.hits);
+        console.log(data.totalHits);
         this.setState((prevState) => ({
-          images: [...prevState.images, ...images],
+          images: [...prevState.images, ...data.hits],
           page: prevState.page + 1,
-          endOfResults: images.length === 0,
-        }))
+          endOfResults: data.hits.length === 0,
+          totalPhoto: data.totalHits,
+        }))}
       )
       .then(this.scroll)
       .catch((error) => this.setState({ error }))
@@ -73,15 +77,18 @@ class App extends Component {
   };
 
   render() {
-    const { images, loading, endOfResults, isModalOpen, selectedImage } = this.state;
+    const { images, page, loading,isModalOpen, selectedImage, totalPhoto } = this.state;
+    console.log((totalPhoto - (page-1) * param.per_page));
+    const restPhoto = (totalPhoto - (page-1) * param.per_page);
+    console.log(restPhoto);
     return (
       <div>
         <SearchBar onSubmit={this.onSearchSubmit} showErrorMessage={images.length === 0} />
         <ImageGallery images={images} openModal={this.openModal} />
         {loading && <Loader />}
         {isModalOpen && <Modal selectedImage={selectedImage} closeModal={this.closeModal} />}
-        {images.length > 0 && !endOfResults && <Button onClick={this.handleClick} />}
-        {endOfResults && images.length > 0 && (
+        {page > 1 && restPhoto > 0 && <Button onClick={this.handleClick} />}
+        {images.length < param.per_page && totalPhoto !==0 && (
           <p className='Sorry'>We're sorry, <br/> but you've reached the end <br/> of search results.</p>
         )}
       </div>
--- src/services/fetchAPI.js
+++ src/services/fetchAPI.js
@@ -1,11 +1,15 @@
-const pixabayKey = '34315621-6b18059d5a6be575efaf0d130';
+export const param = {
+    key: '34315621-6b18059d5a6be575efaf0d130',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: 12,
+  }
 
-const fetchImagesWithQuery = (searchQuery, page=1) => {
-
-    return fetch(`https://pixabay.com/api/?q=${searchQuery}&key=${pixabayKey}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`)
+export const fetchImagesWithQuery = (searchQuery, page=1) => {
+const {per_page, orientation, image_type, key} = param;
+    return fetch(`https://pixabay.com/api/?q=${searchQuery}&key=${key}&page=${page}&image_type=${image_type}&orientation=${orientation}&per_page=${per_page}`)
             .then(res => res.json())
-            .then(data => data.hits);
+            .then(data => data);
 
 }
-
-export default fetchImagesWithQuery;
